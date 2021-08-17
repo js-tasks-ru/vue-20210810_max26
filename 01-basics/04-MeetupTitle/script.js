7a566d38cdf38ff45f3a2ac014de13e44b337f6a
Корольков Максим <m.korolkov@etpgpb.ru>
2021-08-18 02:01:18 +0300

    01-04 meetup title

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,18 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      meetupRadio: '',
+      meetupTitle: '',
+    };
+  },
+  watch: {
+    meetupRadio(newValue) {
+      fetchMeetupById(newValue).then((result) => {
+        this.meetupTitle = result.title;
+      });
+    },
+  },
+}).mount('#app');
